ac73e3f48d5328cb49359f1bbc8794870ac5e173
Arsh Tiwari <tiwariarsh348@gmail.com>
2026-09-10 17:45:00 +0530

    db model

---
+++ lecture_07/src/models/video.model.js
@@ -0,0 +1,46 @@
+import mongoose, { Schema } from "mongoose";
+import mongooseAggregatePaginate  from "mongoose-paginate-v2"
+
+const videoSchema = new Schema(
+    {
+        vedioFile :{
+            type : String, // cloudnary url
+            required : true
+        },
+        thumbnail :{
+            type : String, // cloudnary url
+            required : true
+        },
+        title:{
+            type : String,
+            required : true
+        },
+        discription:{
+            type : String,
+            required : true
+        },
+        duration:{
+            type : Number,
+            required : true
+        },
+        view:{
+            type :Number,
+            default :0
+        },
+        isPublished :{
+            type : Boolean,
+            default : true
+        },
+        owner:{
+            type : mongoose.Schema.Types.ObjectId,
+            ref : "User"
+        }
+    },
+    {
+        timestamps : true
+    }
+)
+
+
+videoSchema.plugin(mongooseAggregatePaginate)
+export const Video = mongoose.model("Video",videoSchema)
