3b872c1f3f36a9ff0115f5cad5e65cb854051d66
Arsh Tiwari <tiwariarsh348@gmail.com>
2026-09-17 13:05:46 +0530

    debuging

--- lecture_07/src/controllers/user.controller.js
+++ lecture_07/src/controllers/user.controller.js
@@ -471,57 +471,63 @@ const getUserChannelProfile = asynchandler(async (req, res) => {
 
 // to getUser video history
 
-const getWatchHistory = asynchandler(async(res,req)=>{
-    const user = await User.aggregate(
-        [
+const getWatchHistory = asynchandler(async (req, res) => {
+
+    const user = await User.aggregate([
         {
-            $match :{
-                _id : new mongoose.Types.ObjectId(req.user._id)
-            },
+            $match: {
+                _id: new mongoose.Types.ObjectId(req.user._id)
+            }
         },
         {
-            $lookup:{
-                form : "videos",
-                localField : "watchHistory",
-                foreignField : "_id",
-                as : "watchHistory",
-                pipeline :[
+            $lookup: {
+                from: "videos",
+                localField: "watchHistory",
+                foreignField: "_id",
+                as: "watchHistory",
+
+                pipeline: [
                     {
-                        $lookup :
-                    {
-                        from : "users",
-                        localField : "owner",
-                        foreignField : "_id",
-                        as : "owner",
-                        pipeline :[
-                            {
-                                $project :{
-                                    fullname :1,
-                                    username : 1,
-                                    avatar :1
+                        $lookup: {
+                            from: "users",
+                            localField: "owner",
+                            foreignField: "_id",
+                            as: "owner",
+
+                            pipeline: [
+                                {
+                                    $project: {
+                                        fullname: 1,
+                                        username: 1,
+                                        avatar: 1
+                                    }
                                 }
-                            }
-                        ]
-                    }
-                },
-                {
-                    $addFields :
+                            ]
+                        }
+                    },
+
                     {
-                        $first:"$owner"
+                        $addFields: {
+                            owner: {
+                                $first: "$owner"
+                            }
+                        }
                     }
-                }
                 ]
             }
         }
-    ])
+    ]);
+
     return res
-    .status(200)
-    .json(new ApiResponce(
-        200,
-        user[0].watchHistory,
-        "watch history fetched successfully"
-    ))
-})
+        .status(200)
+        .json(
+            new ApiResponce(
+                200,
+                user[0].watchHistory,
+                "Watch history fetched successfully"
+            )
+        );
+});
 
 
 export{registerUser,
